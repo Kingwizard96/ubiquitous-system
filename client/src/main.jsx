6089286b0129b8be7575542b45c626224d5e774d
Kingwizard96 <calvarado1996@gmail.com>
2024-02-23 22:24:12 -0600

    front end dev

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,3 @@
-import ReactDom from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-
-import App from './App.jsx';
-import Home from './pages/Home';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
